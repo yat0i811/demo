27d1ce5c94da61205ff4ddf38ca9c06f91223b0e
yatoi <sougo811@outlook.jp>
2024-06-22 02:45:05 +0900

    2typevalue02

--- codehack/views.py
+++ codehack/views.py
@@ -10,19 +10,20 @@ def index(request):
 
 @csrf_exempt
 def post(request):
-  print(request.method)
   if request.method == 'POST':
-    key = request.POST.get('key', None)
-    if key is not None:
+    value01 = request.POST.get('value01', None)
+    value02 = request.POST.get('value02', None)
+    if value01 is not None and value02 is not None:
       try:
-        key = int(key)  # 文字列を整数に変換します
+        value01 = int(value01)  # 文字列を整数に変換します
+        value02 = int(value02)  # 文字列を整数に変換します
         # データを処理します（例：データベースに保存するなど）
-        context = Post(text=key)
+        context = Post(fields={'value01': value01, 'value02': value02})
         context.save()
-        return JsonResponse({'status': 'success', 'key': key})
+        return JsonResponse({'status': 'success', 'value01': value01, 'value02': value02})
       except ValueError:
         return JsonResponse({'status': 'error', 'message': 'Invalid integer value'})
     else:
-      return JsonResponse({'status': 'error', 'message': 'Key not provided'})
+      return JsonResponse({'status': 'error', 'message': 'Value not provided'})
   else:
     return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
