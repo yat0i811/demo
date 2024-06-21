ec6d54d2de078a24828651d7bcd2adc9c7740f7b
yatoi <sougo811@outlook.jp>
2024-06-22 01:19:44 +0900

    json update

--- codehack/views.py
+++ codehack/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 import json
 from .models import Post
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     return render(request, 'codehack/index.html', {'posts': posts})
 
+@require_POST
 def post(request):
     datas = json.loads(request.body)
     context = Post(fields=datas)
     context.save()
-    return render(request, 'codehack/post.html')
+    data = {
+        'message': '登録完了'
+    }
+    return JsonResponse(
+        data=data
+    )
