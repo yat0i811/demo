fd68710de23d9a028ed781010800a943515efe57
yatoi <sougo811@outlook.jp>
2024-06-22 02:15:01 +0900

    post fix03

--- codehack/views.py
+++ codehack/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
-import json
 from .models import Post
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
     return render(request, 'codehack/index.html', {'posts': posts})
 
+@csrf_exempt
 def post(request):
-    datas = json.loads(request.body)
-    context = Post(text=datas['value'])
-    context.save()
-    data = {
-        'message': '登録完了'
-    }
-    return JsonResponse(
-        data=data
-    )
+  print(request.method)
+  if request.method == 'POST':
+    key = request.POST.get('key', None)
+    if key is not None:
+      try:
+        key = int(key)  # 文字列を整数に変換します
+        # データを処理します（例：データベースに保存するなど）
+        context = Post(text=key)
+        context.save()
+        return JsonResponse({'status': 'success', 'key': key})
+      except ValueError:
+        return JsonResponse({'status': 'error', 'message': 'Invalid integer value'})
+    else:
+      return JsonResponse({'status': 'error', 'message': 'Key not provided'})
+  else:
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
